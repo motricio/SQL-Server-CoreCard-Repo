67e1fbdc5b843bb690064c93ccd84eb9d23dd536
YoasmyOz <152532578+YoasmyOz@users.noreply.github.com>
2023-12-01 14:52:34 -0500

    Add files via upload

---
+++ PPG Project/Production/Storage Provisionig/December_1st_2023/006.InsertCappedFilesExclude_CoreAuth.sql
@@ -0,0 +1,10 @@
+Insert into admin.dbo.t_dba_space_checkfordatabasefreespace_exclusion([File Name],[Database Name], Included_Excluded)
+SELECT 'CoreAuth_FGIDX1_Data1_2','CoreAuth',1
+UNION ALL SELECT 'CoreAuth_FGIDX2_Data1_2','CoreAuth',1
+UNION ALL SELECT 'CoreAuth_FGIDX3_Data1_2','CoreAuth',1
+UNION ALL SELECT 'CoreAuth_FGIDX4_Data1_2','CoreAuth',1
+UNION ALL SELECT 'CoreAuth_FGIDX5_Data1_2','CoreAuth',1
+UNION ALL SELECT 'CoreAuth_FGIDX6_Data1_2','CoreAuth',1
+UNION ALL SELECT 'CoreAuth_FGIDX7_Data1_2','CoreAuth',1
+UNION ALL SELECT 'CoreAuth_Data4','CoreAuth',1
+UNION ALL SELECT 'CoreAuth_SEC_Data1_2','CoreAuth',1
