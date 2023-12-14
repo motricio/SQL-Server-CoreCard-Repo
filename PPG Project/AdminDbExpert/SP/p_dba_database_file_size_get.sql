088730c05f40997e1e38b89a99c44f8ef99255a7
Mauricio Rafael Rodríguez Rebolledo <46229667+motricio@users.noreply.github.com>
2023-12-14 10:13:22 -0500

    Update p_dba_database_file_size_get.sql
    order by and @Physical_name added

--- PPG Project/AdminDbExpert/SP/p_dba_database_file_size_get.sql
+++ PPG Project/AdminDbExpert/SP/p_dba_database_file_size_get.sql
@@ -1,13 +1,10 @@
 USE [Admin]
 GO
-
-/****** Object:  StoredProcedure [dbo].[p_dba_database_file_size_get]    Script Date: 12/14/2023 6:38:17 PM ******/
+/****** Object:  StoredProcedure [dbo].[p_dba_database_file_size_get]    Script Date: 12/14/2023 7:01:02 PM ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
-
 
 
 -- =============================================
@@ -15,12 +12,13 @@ GO
 -- Create date: 20231102
 -- Description:	Table get [t_dba_database_file_size]
 -- =============================================
-CREATE PROCEDURE [dbo].[p_dba_database_file_size_get]
+ALTER PROCEDURE [dbo].[p_dba_database_file_size_get]
 (
 	@ServerName varchar(1000)=null,
 	@DataBaseName varchar(1000)=NULL,
 	@FileName varchar(2000)=null,
 	@FileGroup varchar(2000)=null,
+	@Physical_name varchar(2000)=null,
 	@DateCaptureStart Datetime=null,
 	@DateCaptureEnd Datetime=null
 )
@@ -46,9 +44,9 @@ SELECT [ServerName]
 		AND	([ServerName]= isnull(@ServerName,[ServerName]))
 		AND	([FileName]= isnull(@FileName,[FileName]))
 		AND	([FileGroup] like isnull('%'+@FileGroup+'%',[FileGroup]))
+		AND	([Physical_name] like isnull('%'+@Physical_name+'%',[Physical_name]))
 		AND	([DateCapture] >= isnull(@DateCaptureStart,[DateCapture]))
 		AND	([DateCapture] <= isnull(@DateCaptureEnd,[DateCapture]))
 	order by 
-	[DateCapture] desc
+	[DateCapture] asc
 END
-GO
