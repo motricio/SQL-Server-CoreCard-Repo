a9389dec3d9d4e0f8af02b17de9aa8a754ae7112
Mauricio Rafael Rodríguez Rebolledo <46229667+motricio@users.noreply.github.com>
2023-12-14 10:32:57 -0500

    Create p_dba_collect_active_sessions_resource_usage.sql

---
+++ PPG Project/AdminDbExpert/SP/p_dba_collect_active_sessions_resource_usage.sql
@@ -0,0 +1,35 @@
+USE [Admin]
+GO
+
+/****** Object:  StoredProcedure [dbo].[p_dba_collect_active_sessions_resource_usage]    Script Date: 12/14/2023 7:32:28 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		Mauricio Rodriguez R.
+-- Create date: 2023-12-12
+-- Description:	p_dba_collect_active_sessions_resource_usage
+-- =============================================
+CREATE PROCEDURE [dbo].[p_dba_collect_active_sessions_resource_usage]
+(
+	@SECONDS INT, @SESSION_ID INT
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	DECLARE @i INT = 1;
+	WHILE (@i <= @SECONDS)
+		BEGIN
+		WAITFOR DELAY '00:00:01'
+			exec [p_dba_show_active_sessions_resource_usage] @SESSION_ID
+			print 'Completed time capture Interval #: '+ cast(@i as varchar(2)) +', Date Captured: ' + convert(varchar(30),getdate(),121)
+		SET  @i = @i + 1;
+	END 
+	print 'Completed collect active sessions resource usage.'
+	select * from t_dba_show_active_sessions_resource_usage
+END
+GO
+
