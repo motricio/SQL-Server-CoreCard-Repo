5f78f4b0b89585d85536ccbe81305c6137da31c4
Mauricio Rafael Rodríguez Rebolledo <46229667+motricio@users.noreply.github.com>
2023-12-18 10:25:24 -0500

    Update DBA - Collect - Database Daily Txn Log Usage.sql

--- PPG Project/AdminDbExpert/Jobs/DBA - Collect - Database Daily Txn Log Usage.sql
+++ PPG Project/AdminDbExpert/Jobs/DBA - Collect - Database Daily Txn Log Usage.sql
@@ -1,34 +1,81 @@
-USE [Admin]
+USE [msdb]
 GO
 
-/****** Object:  StoredProcedure [dbo].[p_dba_show_txn_log_usage]    Script Date: 12/15/2023 8:10:34 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-
-
-CREATE PROCEDURE [dbo].[p_dba_show_txn_log_usage]
-(
-	@DATABASE_NAME VARCHAR(20)= Null,
-	@DateCaptureStart Datetime=null,
-	@DateCaptureEnd Datetime=null
-)
-AS
+/****** Object:  Job [DBA - Collect - Database Daily Txn Log Usage]    Script Date: 12/18/2023 6:31:11 PM ******/
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [Database Maintenance]    Script Date: 12/18/2023 6:31:11 PM ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'Database Maintenance' AND category_class=1)
 BEGIN
-		SELECT [DataBaseName]
-			  ,[Log Size(MB)]
-			  ,[Log Space used(%)]
-			  ,[Status]
-			  ,[log_reuse_wait_desc]
-			  ,[recovery_model_desc]
-			  ,[DateCapture]
-		  FROM [dbo].[t_dba_txn_log_usage]
-		where 
-		([DataBaseName] like isnull('%'+@DATABASE_NAME+'%',[DataBaseName]))
-		AND	([DateCapture] >= isnull(@DateCaptureStart,[DateCapture]))
-		AND	([DateCapture] <= isnull(@DateCaptureEnd,[DateCapture]))
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'Database Maintenance'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
 END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'DBA - Collect - Database Daily Txn Log Usage', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=0, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'No description available.', 
+		@category_name=N'Database Maintenance', 
+		@owner_login_name=N'sa', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [p_dba_collect_txn_log_usage]    Script Date: 12/18/2023 6:31:11 PM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'p_dba_collect_txn_log_usage', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=3, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'EXEC p_dba_collect_txn_log_usage', 
+		@database_name=N'Admin', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [purge]    Script Date: 12/18/2023 6:31:11 PM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'purge', 
+		@step_id=2, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'delete from [t_dba_txn_log_usage] where DateCapture < getdate()-180', 
+		@database_name=N'Admin', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'DBA - Collect - Database Daily Txn Log Usage', 
+		@enabled=1, 
+		@freq_type=4, 
+		@freq_interval=1, 
+		@freq_subday_type=8, 
+		@freq_subday_interval=1, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=0, 
+		@active_start_date=20231215, 
+		@active_end_date=99991231, 
+		@active_start_time=0, 
+		@active_end_time=235959, 
+		@schedule_uid=N'8ae36d02-12d2-45a8-9d59-3b0445fbe8a9'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
 GO
